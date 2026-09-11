42510cb0dd4721151b4211a6b222850f9f50d628
Karen <123015338+karensnb@users.noreply.github.com>
2026-09-11 17:10:22 +0000

    Tentando arrumar erro de deploy, tentativa 3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import publicRoutes from './routes/public.js';
 import privateRoutes from './routes/private.js';
+import otpRoutes from './routes/otp.js';
 import cors from 'cors'
 
 import auth from './middlewares/auth.js';
@@ -17,6 +18,7 @@ app.use('/API',publicRoutes);
 
 //Essas rotas exigiram que o login tenha sido realizado
 app.use('/API', auth, privateRoutes);
+app.use('/API', otpRoutes)
 
 const PORT = process.env.PORT || 3000;
 
