ff0aae177d62e97d4e1f9d1065772640855c9775
Karen <123015338+karensnb@users.noreply.github.com>
2026-09-21 11:56:36 +0000

    teste

--- routes/public.js
+++ routes/public.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import supabase from '../config/supabase.js';
-import { validarEmailPermitido } from '../utils/email.js'; // ajuste o caminho
+import { validarEmailPermitido} from '../utils/email.js'; 
 
 const router = express.Router();
 
